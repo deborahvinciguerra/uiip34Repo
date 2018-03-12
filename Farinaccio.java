b9ec50c6541312d687f2619ff187d57e54fce78b
Francesca Farinaccio <f.farinaccio@outlook.it>
2018-03-12 23:24:18 +0100

    My file java

---
+++ Farinaccio.java
@@ -0,0 +1,11 @@
+public class StampaNome{
+	
+	public String nome;
+	public String cognome;
+	
+	nome="Francesca";
+	cognome="Farinaccio";
+	
+	System.out.println("Hello World! I'm " + nome + cognome);
+	
+}
