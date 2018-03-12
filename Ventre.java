a4b192c4b42e56949756a5919d2a6c565a21a63b
Carla Ventre <ventrecarla0@gmail.com>
2018-03-12 23:24:45 +0100

    stampanome

---
+++ Ventre.java
@@ -0,0 +1,13 @@
+
+public class Ventre
+{
+
+	public static void main(final String[] args)
+	{
+		final String name = "Carla";
+		final String surname = "Ventre";
+		System.out.println(name + " " + surname);
+
+	}
+
+}
