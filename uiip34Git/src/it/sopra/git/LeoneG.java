1bd2f6a3fb725beabd5857826fb30c214ab28910
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-13 16:54:03 +0100

    ordine inverso

---
+++ uiip34Git/src/it/sopra/git/LeoneG.java
@@ -0,0 +1,12 @@
+package it.sopra.git;
+
+public class LeoneG {
+
+	public static void main(String[] args) {
+		System.out.println("Ventre Carla");
+		System.out.println("Leone Gian Luigi");
+		System.out.println("D'angela Daniele");
+
+	}
+
+}
