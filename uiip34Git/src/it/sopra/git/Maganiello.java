16c9faf5adb5ad3de99f6df55f1c9636799aa1aa
Gianluca Cocco <gianlucacocco8@gmail.com>
2018-03-13 15:40:49 +0100

    gianluca

--- uiip34Git/src/it/sopra/git/Maganiello.java
+++ uiip34Git/src/it/sopra/git/Maganiello.java
@@ -3,6 +3,9 @@ package it.sopra.git;
 public class Maganiello {
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Gianluca Cocco");
+
 		System.out.println("Angelo MAnganiello");
 	}
 
