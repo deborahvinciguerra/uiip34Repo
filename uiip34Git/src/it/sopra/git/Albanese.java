93cc12d8d48e180c47c93c7cb0f113633dd28b88
Francesca Farinaccio <f.farinaccio@outlook.it>
2018-03-13 17:07:41 +0100

    modifiche

--- uiip34Git/src/it/sopra/git/Albanese.java
+++ uiip34Git/src/it/sopra/git/Albanese.java
@@ -3,15 +3,10 @@ public class Albanese {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		System.out.println("Albanese ");
 		System.out.println("Farinaccio ");
 		System.out.println("Franza ");
-=======
-		System.out.println("Franza Andrea");
-		System.out.println("Farinaccio Francesca");
-		System.out.println("Albanese Natalia");
->>>>>>> modifiche
+
 		
 	}
 	
