9c67e32f964b3f0fcc87b2b16090c98b65cbe739
Eugenio Trodella <eu.trodella@gmail.com>
2018-03-13 15:43:30 +0100

    modifica Manganiello.java

--- uiip34Git/src/it/sopra/git/Maganiello.java
+++ uiip34Git/src/it/sopra/git/Maganiello.java
@@ -3,8 +3,8 @@ package it.sopra.git;
 public class Maganiello {
 	
 	public static void main(String[] args) {
-		System.out.println("Cocco Gianluca");
-		System.out.println("Manganiello Angelo");
+
+		System.out.println("Angelo Manganiello");
 		System.out.println("Trodella Eugenio");
 	}
 
