cea1b924b27a72ed17e385f5bdfcfa64dc6d77dd
Carla Ventre <ventrecarla0@gmail.com>
2018-03-13 21:21:04 +0100

    inseriscoNome

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -16,8 +16,9 @@ public class PrintName {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
 		System.out.println(myNameIs("Daniele", "D'angela"));
-		System.out.println(myNameIs("Deborah", "Vinciguerra"));
 		System.out.println(myNameIs("Carla","Ventre"));
+		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+		
 		
 	}
 	
