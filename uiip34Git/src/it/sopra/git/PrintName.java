6e105c69b0502c342fd026c26507cf49579d7dd6
Andrea Franza <a.franza@protonmail.com>
2018-03-13 22:48:47 +0100

    zzz

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -15,8 +15,8 @@ public class PrintName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
-		System.out.println(myNameIs("Deborah", "Vinciguerra"));
 		System.out.println(myNameIs("Andrea", "Franza"));
+		System.out.println(myNameIs("Deborah", "Vinciguerra"));
 		System.out.println(myNameIs("Natalia", "Albanese"));
 	}
 	
