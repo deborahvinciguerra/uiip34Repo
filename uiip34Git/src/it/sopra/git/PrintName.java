a7c00d03f47112730fc1e9e92b2f06d2191f42cf
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-13 21:08:33 +0100

    insert name

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -15,7 +15,9 @@ public class PrintName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
+		System.out.println(myNameIs("Daniele", "D'angela"));
 		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+		
 	}
 	
 	private static String myNameIs(String nome, String cognome){
