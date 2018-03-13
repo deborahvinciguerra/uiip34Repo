9abe88f1ce01120ab6adf9803b08a06e04e7e2d5
Carla Ventre <ventrecarla0@gmail.com>
2018-03-13 10:45:10 +0100

    Ventre.java

--- Ventre.java
+++ Ventre.java
@@ -4,9 +4,7 @@ public class Ventre
 
 	public static void main(final String[] args)
 	{
-		final String name = "Carla";
-		final String surname = "Ventre";
-		System.out.println(name + " " + surname);
+		System.out.println("Carla Ventre");
 
 	}
 
