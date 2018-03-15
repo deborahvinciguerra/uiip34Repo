e2f101d7a1b495813193482f6e06562b57fdce05
Gianluca Cocco <gianlucacocco8@gmail.com>
2018-03-15 17:55:10 +0100

    Gianlucas

--- uiip34Git/src/it/sopra/git/GianlucaCocco.java
+++ uiip34Git/src/it/sopra/git/GianlucaCocco.java
@@ -1,13 +1,34 @@
 package it.sopra.git;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 public class GianlucaCocco {
 
 	public static void main(String[] args) {
-
-		String nome = "Gianluca";
-		String cognome = "Cocco";
-
-		System.out.println(nome + " " + cognome);
+		
+			}
+	public static int calcolaEta() {
+		
+		
+	String nome = "Gianluca";
+	String cognome = "Cocco";
+    int annoNascita = 1991;
+	
+	
+	/*GregorianCalendar calendar=new GregorianCalendar(TimeZone.getDefault());
+	System.out.println(new SimpleDateFormat().format(Calendar.getInstance().getTime()));
+	*/
+	
+	
+    
+	int year = Calendar.getInstance().get(Calendar.YEAR);
+//	System.out.println("La mia età è \n" + (year - annoNascita) + " anni");
+	
+	return ( year - annoNascita);
+	
 	}
-
+	
 }
--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -15,7 +15,9 @@ public class PrintName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
+		System.out.println(myNameIs("Gianluca", "Cocco") + " "+ GianlucaCocco.calcolaEta() + " anni");
 		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+
 	}
 	
 	private static String myNameIs(String nome, String cognome){
