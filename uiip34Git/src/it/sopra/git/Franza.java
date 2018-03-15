e4a3e3a5b4dd3a40074c3d3553cea3103e02570b
Andrea Franza <a.franza@protonmail.com>
2018-03-15 20:32:23 +0100

    zzz

--- uiip34Git/src/it/sopra/git/Franza.java
+++ uiip34Git/src/it/sopra/git/Franza.java
@@ -1,10 +1,25 @@
 package it.sopra.git;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Franza {
 
+	String nome = "Andrea";
+	String cognome = "Franza";
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Il mio nome è Andrea Franza");
 	}
-
+	
+	public static String calcolaEta (int anno, int mese, int giorno)
+	{
+		Date oggi = new GregorianCalendar(anno, mese, anno).getTime();
+		Date nascita = new GregorianCalendar(1991, 7, 23).getTime();
+		Long eta = nascita.getTime() - oggi.getTime();
+		eta = eta/(1000*60*60*24*365);
+		return eta.toString();
+	}
 }
--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -3,6 +3,9 @@
  */
 package it.sopra.git;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * @author dvinciguerra
  *
@@ -16,6 +19,9 @@ public class PrintName {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
 		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+		System.out.println(GregorianCalendar. + /*GregorianCalendar.MONTH*/ + GregorianCalendar.DAY_OF_MONTH);
+		System.out.println(myNameIs("Andrea", "Franza")+" "+Franza.calcolaEta(GregorianCalendar.YEAR, GregorianCalendar.MONTH, GregorianCalendar.DAY_OF_MONTH));
+		
 	}
 	
 	private static String myNameIs(String nome, String cognome){
