f58abf4a024865cd105a89f5d9c9e2aae7e9fe35
Angelo Manganiello <angelo.manganiello86@gmail.com>
2018-03-15 17:52:44 +0100

    modifica a Manganiello.java

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -21,20 +21,9 @@ public class PrintName {
 		System.out.println(myNameIs("Carla", "Ventre"));
 		LocalDate birthDate = LocalDate.of(1992,06,19);
 		System.out.println(Ventre.calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
-		System.out.println(myNameIs("Angelo", "Manganiello"));
-		System.out.println(Maganiello.calcolaEta(new GregorianCalendar(1986,11,8)));
-<<<<<<< HEAD
-<<<<<<< HEAD
+		System.out.println(myNameIs("Angelo", "Manganiello")+ " " + Maganiello.calcolaEta(new GregorianCalendar(1986,11,8)));
 		System.out.println(myNameIs("Eugenio", "Trodella") + " "+Trodella.calcolaEta());
 		System.out.println(myNameIs("Deborah", "Vinciguerra"));
-		
-
-		
-=======
->>>>>>> 73095fd923fbb8d291a704356aeb1afb42dc7a28
-
-=======
->>>>>>> 868dc1700073263195f7e15474dfd4ff646de466
 	}
 	
 	private static String myNameIs(String nome, String cognome){
