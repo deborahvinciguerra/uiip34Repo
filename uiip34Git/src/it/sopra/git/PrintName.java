0f9cda3658f1bd7aa4daae606c94be939e3361df
Carla Ventre <ventrecarla0@gmail.com>
2018-03-15 17:51:18 +0100

    eta

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -20,7 +20,7 @@ public class PrintName {
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
 		System.out.println(myNameIs("Carla", "Ventre"));
 		LocalDate birthDate = LocalDate.of(1992,06,19);
-		System.out.println(Ventre.calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
+		System.out.println(myNameIs("Carla", "Ventre") + "," + Ventre.calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
 		System.out.println(myNameIs("Angelo", "Manganiello"));
 		System.out.println(Maganiello.calcolaEta(new GregorianCalendar(1986,11,8)));
 	}
