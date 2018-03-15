e81517951b27e9a068e099eba8c358e579830717
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-15 17:53:37 +0100

    calcolaEta

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -3,6 +3,8 @@
  */
 package it.sopra.git;
 
+import java.time.LocalDate;
+
 /**
  * @author dvinciguerra
  *
@@ -15,7 +17,10 @@ public class PrintName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
+		System.out.println(myNameIs("Daniele","D'angela") + "---" + Dangela.calcolaEta(LocalDate.of(1990, 9, 21)));
 		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+		
+		
 	}
 	
 	private static String myNameIs(String nome, String cognome){
