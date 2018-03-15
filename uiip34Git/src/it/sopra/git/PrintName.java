d5723140aecae711a2cba5180ae9c8629d0d70a7
Carla Ventre <ventrecarla0@gmail.com>
2018-03-15 17:38:15 +0100

    eta

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -3,11 +3,13 @@
  */
 package it.sopra.git;
 
+import java.time.LocalDate;
+
 /**
  * @author dvinciguerra
  *
  */
-public class PrintName {
+public class PrintName extends Ventre {
 
 	/**
 	 * @param args
@@ -15,13 +17,18 @@ public class PrintName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
-		System.out.println(myNameIs("Deborah", "Vinciguerra"));
+		System.out.println(myNameIs("Carla", "Ventre"));
+		
+		LocalDate birthDate = LocalDate.of(1992,06,19);
+		System.out.println(calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
 	}
 	
 	private static String myNameIs(String nome, String cognome){
 		return cognome+" "+nome;
 		
 	}
+	
+	
 
 
 }
--- uiip34Git/src/it/sopra/git/Ventre.java
+++ uiip34Git/src/it/sopra/git/Ventre.java
@@ -1,10 +1,32 @@
 package it.sopra.git;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Ventre {
 
 	public static void main(String[] args) {
 		System.out.println("Carla Ventre");
 
 	}
+	
+	
+	/*public static void calcolaEta (GregorianCalendar dataNascita) {
+		int eta=0;
+		int giorno =19;
+		int mese=06;
+		int anno=1992;
+		dataNascita = new GregorianCalendar(giorno,mese,anno);
+		GregorianCalendar now = new GregorianCalendar();
+		eta=(now.get(GregorianCalendar.YEAR) - dataNascita.get(GregorianCalendar.YEAR));
+	}*/
 
+	
+	public static int calcolaEta(LocalDate birthDate, LocalDate currentDate) {
+        if ((birthDate != null) && (currentDate != null)) {
+            return Period.between(birthDate, currentDate).getYears();
+        } else {
+            return 0;
+        }
+}
 }
