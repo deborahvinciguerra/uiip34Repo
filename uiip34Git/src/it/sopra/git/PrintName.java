868dc1700073263195f7e15474dfd4ff646de466
Carla Ventre <ventrecarla0@gmail.com>
2018-03-15 17:47:00 +0100

    eta

--- uiip34Git/src/it/sopra/git/PrintName.java
+++ uiip34Git/src/it/sopra/git/PrintName.java
@@ -3,17 +3,14 @@
  */
 package it.sopra.git;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
-=======
 import java.util.GregorianCalendar;
->>>>>>> 215b9b0a6839288be3cb57e46c00f0e6232e49fe
 
 /**
  * @author dvinciguerra
  *
  */
-public class PrintName extends Ventre {
+public class PrintName {
 
 	/**
 	 * @param args
@@ -23,10 +20,9 @@ public class PrintName extends Ventre {
 		System.out.println("<<<<<ELENCO ISCRITTI IN ORDINE ALFABETICO>>>>>>");
 		System.out.println(myNameIs("Carla", "Ventre"));
 		LocalDate birthDate = LocalDate.of(1992,06,19);
-		System.out.println(calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
+		System.out.println(Ventre.calcolaEta(birthDate,LocalDate.of(2018,03,15))+"anni");
 		System.out.println(myNameIs("Angelo", "Manganiello"));
 		System.out.println(Maganiello.calcolaEta(new GregorianCalendar(1986,11,8)));
-
 	}
 	
 	private static String myNameIs(String nome, String cognome){
