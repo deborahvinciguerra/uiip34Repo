1e81dc4d66ad4c4c9ce0233db813473c982fbf9c
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-16 11:52:39 +0100

    modify project

--- viaggi/src/main/java/it/sopra/uiip/viaggi/App.java
+++ viaggi/src/main/java/it/sopra/uiip/viaggi/App.java
@@ -2,36 +2,31 @@ package it.sopra.uiip.viaggi;
 
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Vector;
 
-import it.sopra.uiip.DAO.DAO;
+import it.sopra.uiip.viaggi.dao.impl.DaoImpl;
+import it.sopra.uiip.viaggi.model.Aereo;
 
 
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-    	String url = "jdbc:mysql://localhost:3306/viaggi";
-    	String username = "root";
-    	String password = "root";
-
-    	System.out.println("Connecting database...");
-
-    	try (Connection connection = DriverManager.getConnection(url, username, password)) {
-    	    System.out.println("Database connected!");
-    	    DAO.insertAereo(connection);
-    	    
-    	} catch (SQLException e) {
-    	    throw new IllegalStateException("Cannot connect the database!", e);
-    }
-    	
+    	 try {
+    		 DbAccess db = new DbAccess();
+    	 Connection conn = db.connect();
+    	 Vector<Aereo> lista_aereo  = DaoImpl.getAereo(conn);
+ 	    
+	     for(Aereo a : lista_aereo) {
+	    	 System.out.println(a.toString());
+	     }
+	    
+	     conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     	
     }
 }
