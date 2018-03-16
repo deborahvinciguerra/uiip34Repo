591daa515567b5d28a9cccd04f312c0baebe5b5c
Angelo Manganiello <angelo.manganiello86@gmail.com>
2018-03-16 12:31:46 +0100

    implementata classe JdbcAeroporto.java

--- viaggi/src/main/java/it/sopra/uiip/libeccio/viaggi/model/daoimpl/JdbcAeroportoDAO.java
+++ viaggi/src/main/java/it/sopra/uiip/libeccio/viaggi/model/daoimpl/JdbcAeroportoDAO.java
@@ -1,24 +1,65 @@
 package it.sopra.uiip.libeccio.viaggi.model.daoimpl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import it.sopra.uiip.libeccio.viaggi.model.Aeroporto;
 import it.sopra.uiip.libeccio.viaggi.model.dao.AeroportoDAO;
 
 public class JdbcAeroportoDAO implements AeroportoDAO {
 
 	public void insert(Aeroporto aeroporto) {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		Statement stm = null;
+		try {
+			conn = ConnectionManager.getConnection();
+			stm = conn.createStatement();
+			String query = "INSERT INTO aeroporto VALUE ("+aeroporto.getCitta()+","+
+			aeroporto.getNazione()+","+aeroporto.getPiste()+")";			
+			stm.executeUpdate(query);
+			conn.close();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Aeroporto selectByCitta(String citta) {
+		Connection conn = null;
+		Statement stm = null ;
+		ResultSet rs = null;
+		String query = "SELECT * FROM aeroporto WHERE citta="+citta;
+		try {
+			rs = stm.executeQuery(query);
+			if (rs.next() ) {
+				String cit = rs.getString("citta");
+				String naz =rs.getString("nazione");
+				int pis = rs.getInt("npiste");
+				return new Aeroporto(cit,naz,pis) ;
+			}			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	public void update(String citta, Aeroporto aeroporto) {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		Statement stm = null;
+		try {
+			conn = ConnectionManager.getConnection();
+			stm = conn.createStatement();
+			String query = "UPDATE aeroporto SET(citta="+aeroporto.getCitta()+
+					",nazione="+aeroporto.getNazione()+
+					",npiste="+aeroporto.getPiste()+"where citta="+aeroporto.getCitta();			
+			stm.executeUpdate(query);
+			conn.close();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
