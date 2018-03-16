d7a8b1a63159e67c34a894c601853a122a6a5cf8
Natalia Albanese <nataliaalbanese694@gmail.com>
2018-03-16 14:16:32 +0100

    implementazione

--- ConnectionVoli/src/main/java/dao/impl/JDBCAeroportoDAO.java
+++ ConnectionVoli/src/main/java/dao/impl/JDBCAeroportoDAO.java
@@ -1,12 +1,47 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import dao.AeroportoDAO;
 import model.Aeroporto;
 
 public class JDBCAeroportoDAO implements AeroportoDAO {
 
-	public void inserisci(Aeroporto aeroporto) {
-		// TODO Auto-generated method stub
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	public void inserisci(Aeroporto a) {
+
+		String sql = "INSERT INTO aeroporto " +
+				"(citta, nazione, nPiste) VALUES (?, ?, ?)";
+		Connection conn = null;
+
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, a.getCitta());
+			ps.setString(2, a.getNazione());
+			ps.setInt(3, a.getNpiste());
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
 		
 	}
 
