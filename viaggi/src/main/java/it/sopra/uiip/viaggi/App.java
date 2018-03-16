08913c0836af0c1a1a46370332e079b8d85a7360
Daniele d'Angela <dangela.daniele@hotmail.it>
2018-03-16 10:22:52 +0100

    carica project

---
+++ viaggi/src/main/java/it/sopra/uiip/viaggi/App.java
@@ -0,0 +1,13 @@
+package it.sopra.uiip.viaggi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
